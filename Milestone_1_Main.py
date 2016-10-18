230b578b01b1da7d3b1b27bd49fea7b53f7cec9a
brohan <BrianJRohan@gmail.com>
2016-10-18 15:04:11 -0700

    Test

--- Milestone_1_Main.py
+++ Milestone_1_Main.py
@@ -1,8 +1,13 @@
 
 
 board = [[1,2,3],[4,5,6],[7,8,9]]
+def print_board(board):
+    for column in board:
+        for row in column:
+            print(row, end='')
+        print(end='\n')
 
-for column in board:
-    for row in column:
-        print(row, end='')
-    print(end='\n')
+def verify_input_player1(input):
+    if input in [1,2,3,4,5,6,7,8,9]:
+        update_board_player1(input)
+    elif if input == "reset":
