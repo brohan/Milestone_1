bbc3f73dc4e1538ec18a3957700db45da154458b
brohan <BrianJRohan@gmail.com>
2016-10-19 14:51:06 -0700

    Test

--- Milestone_1_Main.py
+++ Milestone_1_Main.py
@@ -1,8 +1,8 @@
 #! /usr/bin/python3
 
+
 def clearscreen():
     print("\n" * 100)
-
 
 def print_board(board):
     for row in board:
@@ -12,23 +12,37 @@ def print_board(board):
     print('\n')
 
 def next_turn(which_player):
-    move = input("Enter 'reset' to start over and 'quit' to exit game \nTo move, choose spot to play (1-9).\nPlayer {}, you're up:".format(which_player))
+    move = input("Enter 'quit' to exit game \nTo move, choose spot to play (1-9).\nPlayer {}, you're up: ".format(which_player))
     return move
 
-def verify_input_player1(input):
+def verify_input(input):
     if input in [1,2,3,4,5,6,7,8,9]:
-        update_board_player1(input)
-    #elif input == "reset":
+        return input
+    elif input == "quit":
+        quit()
 
+def make_move(move,player_turn):
+
+    return(player_turn)
 
 clearscreen()
 player_turn = 1
 board = [[1,2,3],[4,5,6],[7,8,9]]
-print('Welcome to the classic Tic Tac Toe game \n \n')
+print('Welcome to the classic game of Tic Tac Toe \n')
 print_board(board)
 while True:
-    next_turn(player_turn)
+    move = next_turn(player_turn)
+    verified = verify_input(move)
+    player_turn = make_move(verified,player_turn)
 
 
-''' use while True: when it gets to the end, it will cycle back to just under this statement, your loop
-Make sure to plant exits when appropriate probably an unindented line that says exit()'''
+'''
+To Do:
+Create something that contains all possible ways to win for either player
+Function to make move:
+    After checking move hasn't been made before
+        If has been made, state so, take input and pass thru verify function
+    Checks if player has won
+    Changes global player variable
+make sure board update takes into account and updates the player
+'''
